56c1971ccbf0b7eac8ccdbc14bfa4d0a41a2310d
adriandamii <adrian.damii@yahoo.com>
2023-05-18 22:09:15 +0300

    Added the delete task functionality

--- POO-BanuIulian-AdrianDamii/FileUtils.h
+++ POO-BanuIulian-AdrianDamii/FileUtils.h
@@ -1,7 +1,58 @@
 #include "pch.h";
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace System::IO;
 using namespace System;
+
+void DeleteLineInFile(const std::string& fileName, int lineNumber, std::string& deletedContent) {
+    // Open the original file for reading
+    std::ifstream inputFile(fileName);
+    if (!inputFile) {
+        std::cout << "Failed to open the file." << std::endl;
+        return;
+    }
+
+    std::string line;
+    int currentLine = 1;
+    std::vector<std::string> lines;
+
+    // Read each line from the original file
+    while (std::getline(inputFile, line)) {
+        // Store the line content except the line to be deleted
+        if (currentLine != lineNumber) {
+            lines.push_back(line);
+        }
+        else {
+            deletedContent = line;
+        }
+        currentLine++;
+    }
+
+    // Close the file
+    inputFile.close();
+
+    // Open the file for writing
+    std::ofstream outputFile(fileName);
+    if (!outputFile) {
+        std::cout << "Failed to open the file for writing." << std::endl;
+        return;
+    }
+
+    // Write the modified content to the file
+    for (const auto& line : lines)
+    {
+        outputFile << line << std::endl;
+    }
+
+    // Close the file
+    outputFile.close();
+    std::cout << "Line " << lineNumber << " deleted from the file." << std::endl;
+}
+
 
 void VerifyAndCreateFile()
 {
