9c7ca45e1eb1339b8d00eee45841005fa03e96ca
adriandamii <adrian.damii@yahoo.com>
2023-05-18 17:07:09 +0300

    Deleted the .vs folder

--- POO-BanuIulian-AdrianDamii/POO-BanuIulian-AdrianDamii.cpp
+++ POO-BanuIulian-AdrianDamii/POO-BanuIulian-AdrianDamii.cpp
@@ -1,7 +1,13 @@
 #include "pch.h"
 
 using namespace System;
-
+ref class Task {
+public:
+	String^ TaskText;
+	bool CompletionStatus;
+	System::DateTime^ CreationTime;
+	System::DateTime^ CompletionTime;
+};
 int main(array<System::String ^> ^args)
 {
 	Console::WriteLine("Hello World!");
